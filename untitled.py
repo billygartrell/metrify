8dbd5e88ca3e28982902da0e7ca3ea138cb54724
Josh Peters <jpeters@listrak.com>
2017-10-17 14:06:33 -0400

    added spotipy dependency

--- untitled.py
+++ untitled.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import spotipy
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    spotify = spotipy.Spotify()
+    results = spotify.search(q='artist: wicca phase springs eternal', type='artist')
+    print(results)
 
 
 if __name__ == '__main__':
